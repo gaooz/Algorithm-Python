dd2414e85a4e5be493a9d7118e3ee7ab566279d7
gaooz.com <5611085@qq.com>
2016-10-08 21:47:18 +0800

    2016-10-08

--- Graphics Algorithm/Find_articul_node.py
+++ Graphics Algorithm/Find_articul_node.py
@@ -3,9 +3,17 @@
 # gaooz.com
 # 关节点算法
 
+# 图中顶点的访问次序
+VISITED_INDEX = 0
+# 存储图中各顶点的访问次序
+visited = []
 
 def find_articul_node(graphics):
     pass
 
 def dfs_articul(graphics, index):
-    pass
+    '''
+    从第index个顶点出发，深度优先遍历图graphics
+    查找并输出关节点
+    '''
+
